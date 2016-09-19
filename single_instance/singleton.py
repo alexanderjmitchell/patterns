1cce2e2736e37a534013aa20519e7a3715b13447
Alex <alex@growthintel.com>
2016-09-19 14:14:30 +0100

    Added borg example

--- single_instance/singleton.py
+++ single_instance/singleton.py
@@ -1,6 +1,6 @@
 class Singleton(object):
 
-    def __new__(cls):
+    def __new__(cls, *args, **kwargs):
         if not hasattr(cls, 'instance'):
             cls.instance = super(Singleton, cls).__new__(cls)
         return cls.instance
